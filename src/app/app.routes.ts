8f45360bc4c27ee673d15e18727baeee08474d53
Wilson Farias Joaquim <wilsonjoaquim2277@gmail.com>
2025-02-12 20:12:43 +0000

    criando a pagina de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 import { Component } from '@angular/core';
 
 export const routes: Routes = [
   {
     path: "login",
     component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
   }
 ];
